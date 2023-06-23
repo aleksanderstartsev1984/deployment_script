6f269e2689d24ea3429e741ebfe7c480b77f9d10
aleksanderstartsev1984 <strami@yandex.ru>
2023-06-23 14:32:09 +0700

    editing script

--- installation_script.sh
+++ installation_script.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/bash
 # Начать индикацию выполнения скрипта
 timer_funce(){
-    wile sleep 
-    do
-    echo -n "*" > &2
-    done
+    while sleep 1; do echo -n "*" > &2; done
 }
 timer_funce &
 timer_funce_pid=$!
